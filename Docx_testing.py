b5f856eb302c4c2ed267671fdbed032183a90550
Arkadii01 <arkadij.t@bk.ru>
2025-01-24 22:35:57 +0700

    Расширение оболочки и рачало работы над .py файлом

--- Docx_testing.py
+++ Docx_testing.py
@@ -1,1 +1,12 @@
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, qApp
 
+class MainWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('main.ui', self)
+        
+if __name__ == '__main__':
+    MainWidget().show()
+    sys.exit(QApplication(sys.argv).exec())
