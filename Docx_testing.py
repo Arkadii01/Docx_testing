71fbc81be6a295b70745a1cf8c10b14f2f14ea14
Arkadii01 <arkadij.t@bk.ru>
2025-01-24 23:55:19 +0700

    Изменение кодировки

--- Docx_testing.py
+++ Docx_testing.py
@@ -1,4 +1,4 @@
-import sys
+﻿import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 import docx_worker
--- docx_worker.py
+++ docx_worker.py
@@ -1,4 +1,4 @@
-from docx import Document
+﻿from docx import Document
 from docx.shared import Inches
 import os
 
