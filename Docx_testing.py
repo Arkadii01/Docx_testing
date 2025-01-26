031599905505de622b489d3c89d44ba2d6ee17d8
Arkadii01 <arkadij.t@bk.ru>
2025-01-26 16:02:23 +0700

    Чтение json-данных

--- Docx_testing.py
+++ Docx_testing.py
@@ -1,8 +1,9 @@
 ﻿import sys
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPlainTextEdit
 import docx_worker
 import json
+import os
 
 class MainWidget(QMainWindow):
     def __init__(self):
@@ -12,13 +13,34 @@ class MainWidget(QMainWindow):
         self.text_error.hide()
         self.btn_save.clicked.connect(self.save)
         self.btn_load.clicked.connect(self.load)
+        os.chdir('data')
+        with open('data.json', 'r', encoding='utf-8') as file:
+            text = ''.join(file.readlines())[1:]
+            data = json.loads(text)
+        os.chdir('../docs_for_test')
+        for file in os.listdir():
+            self.box_filename.addItem(file)
+        self.box_filetype.setCurrentText(data['filetype'])
+        self.input_teacher.appendPlainText(data['teacher'])
+        self.edgewall_up.setValue(data['edge-wall']['top'])
+        self.edgewall_bottom.setValue(data['edge-wall']['bottom'])
+        self.edgewall_left.setValue(data['edge-wall']['left'])
+        self.edgewall_right.setValue(data['edge-wall']['right'])
+        self.walltext_up.setValue(data['wall-text']['top'])
+        self.walltext_bottom.setValue(data['wall-text']['bottom'])
+        self.walltext_left.setValue(data['wall-text']['left'])
+        self.walltext_right.setValue(data['wall-text']['right'])
+        self.fontsize.setValue(data['font-size'])
+        self.rows_distance.setValue(data['rows_distance'])
+        self.first_void.setValue(data['first_void'])
 
     def update(self):
         self.close()
         self.show()
         
     def save(self):
-        pass
+        print(self.edgewall_up.value())
+        print(self.box_filename.currentText())
     
     def load(self):
         try:
